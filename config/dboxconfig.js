52cf002441a4fec0d0fec5ebb2a93a26a6cce9a5
raychowus1 <erayusa@gmail.com>
2018-08-06 13:00:20 -0500

    Create dboxconfig.js

---
+++ config/dboxconfig.js
@@ -0,0 +1,12 @@
+var dboxconfig = {
+ port: 3000,
+ environment: "development",
+ logstatus: "false",
+ loglevel: 1,
+ Appkey: "APP-KEY",
+ Appsecret: "APP-KEY",
+ timezone: "UTC",
+ debug: false
+};
+
+module.exports = dboxconfig;
